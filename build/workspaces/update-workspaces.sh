78357eed6523f57b5beb6b57e3e95a69572e4593
andy5995 <andy400-dev@yahoo.com>
2018-08-13 03:44:58 -0500

    update-workspaces.sh:revert accidental change

--- build/workspaces/update-workspaces.sh
+++ build/workspaces/update-workspaces.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-#export CXXFLAGS=-D_FILE_OFFSET_BITS=64
-#export CFLAGS=-D_FILE_OFFSET_BITS=64
-
-#if [ "$(id -u)" = "0" ]; then
-#   echo "Running as root will mess up file permissions. Aborting ..." 1>&2
-#   exit 1
-#fi
+if [ "$(id -u)" = "0" ]; then
+   echo "Running as root will mess up file permissions. Aborting ..." 1>&2
+   exit 1
+fi
 
 die()
 {

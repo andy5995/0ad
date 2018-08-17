5c880f6cbb27e2a5830a428539701c1d3737e8c6
andy5995 <andy400-dev@yahoo.com>
2018-08-17 04:05:36 -0500

    add deps for libsld2-dev
    https://github.com/travis-ci/travis-ci/issues/9065

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -5,6 +5,10 @@ sudo bash -c "LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php"
 sudo apt-get update
 sudo apt-get install -y libsodium-dev
 
+# required to install libsdl2-dev
+# https://github.com/travis-ci/travis-ci/issues/9065
+sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev
+
 sudo apt-get install libboost-dev libboost-filesystem-dev   \
     libcurl4-gnutls-dev libenet-dev libgloox-dev libicu-dev    \
     libminiupnpc-dev libnspr4-dev libnvtt-dev libogg-dev libopenal-dev   \

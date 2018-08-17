64760b064c263f3429f8e5032ade3e32a264c35f
andy5995 <andy400-dev@yahoo.com>
2018-08-17 03:53:51 -0500

    travis_before_install:add different repo for libsodium

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:chris-lea/libsodium
-sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
-sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
-sudo apt-get update && sudo apt-get install libsodium-dev
+sudo apt-get install -y software-properties-common
+sudo bash -c "LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php"
+sudo apt-get update
+sudo apt-get install -y libsodium-dev
 
-sudo apt-get install libnspr4-dev libsdl2-dev libboost-dev libgloox-dev \
-  libminiupnpc-dev libenet-dev libopenal-dev
-
+sudo apt-get install libboost-dev libboost-filesystem-dev   \
+    libcurl4-gnutls-dev libenet-dev libgloox-dev libicu-dev    \
+    libminiupnpc-dev libnspr4-dev libnvtt-dev libogg-dev libopenal-dev   \
+    libpng-dev libsdl2-dev libvorbis-dev libwxgtk3.0-dev libxcursor-dev  \
+    libxml2-dev zlib1g-dev

02ea260b53f8a2110a992f52ec1bc6d6d48234ab
andy5995 <andy400-dev@yahoo.com>
2018-08-18 16:49:19 -0500

    fix package names, add hyphen to assigned Tr vars

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -4,8 +4,8 @@ sudo apt-get install -y software-properties-common
 #sudo bash -c "LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php"
 sudo bash -c "LC_ALL=C.UTF-8 add-apt-repository -y ppa:wfg/0ad"
 sudo apt-get update
-sudo apt-get install -y libsodium-1.0.16-2~14.04~wfg2 gloox-1.0.18-1~14.04~wfg2 \
-    enet-1.3.12+ds-2~14.04~wfg1 boost1.58-1.58.0+dfsg-5ubuntu3.1~14.04~wfg0
+sudo apt-get install -y libsodium-dev libgloox-dev \
+    libenet-dev libboost1.58-dev libboost-filesystem1.58-dev
 
 # required to install libsdl2-dev
 # https://github.com/travis-ci/travis-ci/issues/9065
